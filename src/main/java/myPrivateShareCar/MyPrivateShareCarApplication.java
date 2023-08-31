a8eb17cf3c69f42f4b2990f3fc614a9520a19e77
Сергей <sosnovskysy@yandex.ru>
2023-08-31 13:33:41 +0600

    Start

---
+++ src/main/java/myPrivateShareCar/MyPrivateShareCarApplication.java
@@ -0,0 +1,13 @@
+package myPrivateShareCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrivateShareCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPrivateShareCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myPrivateShareCar/MyPrivateShareCarApplicationTests.java
@@ -0,0 +1,13 @@
+package myPrivateShareCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPrivateShareCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
